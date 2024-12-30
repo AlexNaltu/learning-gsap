33d045cfbfa991f8b5498ac94c22b71b6bf66a36
Alex Naltu <naltuok@gmail.com>
2024-12-30 06:37:49 +0100

    gsap context

--- src/components/animated-component.tsx
+++ src/components/animated-component.tsx
@@ -52,7 +52,22 @@ const AnimatedComponent = () => {
     gsap.effects.slideOut(".box", { duration: 5 });
   }, []);
 
-  return <div className="w-52 h-52 bg-red-950 box"></div>;
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.to(".box1", { duration: 2, x: 100, ease: "power1.inOut" });
+      gsap
+        .timeline()
+        .to(".box1", { duration: 2, x: 100 })
+        .to(".box1", { duration: 2, x: -200 });
+    });
+  });
+
+  return (
+    <>
+      <div className="w-52 h-52 bg-red-950 box"></div>
+      <div className="w-52 h-52 bg-red-950 box1"></div>
+    </>
+  );
 };
 
 export default AnimatedComponent;
